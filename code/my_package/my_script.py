d9d5722b9d31978e245e255c643b1a562c0990e7
jd-ttic <jduran@ttic.edu>
2019-11-22 16:00:42 -0600

    updates

--- code/my_package/my_script.py
+++ code/my_package/my_script.py
@@ -1,3 +1,7 @@
 import os
-message = "Hello from !".format(os.environ['VEHICLE_Name'])
+import numpy as np
+message = "Hello from {}!".format(os.environ['VEHICLE_NAME'])
 print(message)
+adding = "1 + 1 is {}".format(np.add(1.0,1.0))
+print(adding)
+
